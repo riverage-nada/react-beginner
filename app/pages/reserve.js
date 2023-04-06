9d5cf2cf5bd69958ca0d32754e55c162697f87e1
riverage-nada <62741301+riverage-nada@users.noreply.github.com>
2023-04-07 02:07:27 +0900

    remove

--- app/pages/reserve.js
+++
@@ -1,7 +0,0 @@
-import Reserve from "../src/template/Reserve";
-
-const Reserve_Page = () => {
-  return <Reserve />;
-};
-
-export default Reserve_Page;
--- app/src/comp/HeroImg/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import css from "./HeroImg.module.scss";
-
-const HeroImg = () => {
-  return <img className={`${css["img"]}`} src="/hero.png" alt="" />;
-};
-
-export default HeroImg;
