ef24d1e5b75b634bf476efef66cea9a26e791564
riverage-nada <62741301+riverage-nada@users.noreply.github.com>
2023-04-10 18:55:36 +0900

    change button

--- app/src/comp/Buttton/index.jsx
+++ app/src/comp/Buttton/index.jsx
@@ -2,9 +2,9 @@ import css from "./Button.module.scss";
 
 const Button = (props) => {
   return (
-    <button className={`${css["comp"]}`} type={props.type || "button"}>
-      {/* / props.typeがなければ"button"が設定される */}
-      {/* / <button>はformの中にある時、自動でtype="submit"になる。そうするとカスタマイズしにくいので、submitにしたい場合のみ呼び出しもとから設定できるようにしている */}
+    <button className={`${css["comp"]}`} {...props}>
+      {/*  {...props} はスプレッド構文という書き方*/}
+      {/* この書き方で、propsの中身を展開している。この書き方をすることで、このコンポーネントを呼び出す際に、このコンポーネントに渡したいpropsを、このコンポーネントの引数としてそのまま渡すことができる */}
       <span className={`${css["text"]}`}>{props.text}</span>
     </button>
   );
